11001c8480608dce959a4e949d36888a2756a7e7
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-10-30 15:42:14 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,8 @@ from blog.models import Post, Tag, Comment
 
 admin.site.register(Post)
 admin.site.register(Tag)
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    raw_id_fields = ['post']
+    list_display = ('post', 'author', 'text', 'published_at')
